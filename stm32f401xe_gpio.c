e8643471b0dc8d838b90e61d20f596028c58fc6b
vidhyamg <deepuvidhya@gmail.com>
2026-09-16 15:14:53 +0530

    Enhance GPIO_Init to configure alternate function mode
    Added support for alternate function mode in GPIO initialization.

--- stm32f401xe_gpio.c
+++ stm32f401xe_gpio.c
@@ -112,6 +112,16 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
                 pGPIOHandle->pGPIOx->OTYPER &= ~(0x1
                         << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
                 pGPIOHandle->pGPIOx->OTYPER |= temp;
+           //5.
+	 if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+                {
+                    uint8_t temp1, temp2;
+                    temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;
+                    temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8;
+                    pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0xF << (4 * temp2));
+                    pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4 * temp2));
+                }
+
 }
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 {
